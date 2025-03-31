0254e0dbba6319c3bbd8c735de9754fa856029c8
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-03-31 22:13:48 +0000

    tenant create ctr

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -1,18 +1,18 @@
 import express from "express";
-// import {
-//   getTenant,
-//   createTenant,
+import {
+  getTenant,
+  createTenant,
 //   updateTenant,
 //   getCurrentResidences,
 //   addFavoriteProperty,
 //   removeFavoriteProperty,
-// } from "../controllers/tenantControllers";
+} from "../controllers/tenantControllers";
 
 const router = express.Router();
 
-// router.get("/:cognitoId", getTenant);
+router.get("/:cognitoId", getTenant);
 // router.put("/:cognitoId", updateTenant);
-// router.post("/", createTenant);
+router.post("/", createTenant);
 // router.get("/:cognitoId/current-residences", getCurrentResidences);
 // router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
 // router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
