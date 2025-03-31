62e395dc63fad571a668f9389caa632ecb6127f8
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-03-31 18:47:06 +0000

    Discover section

--- front/src/app/(nondashbord)/landing/DiscoverSection.tsx
+++ front/src/app/(nondashbord)/landing/DiscoverSection.tsx
@@ -26,24 +26,24 @@ const DiscoverSection = () => {
       whileInView="visible"
       viewport={{ once: true, amount: 0.8 }}
       variants={containerVariants}
-      className="!py-12 bg-white !mb-16"
+      className="py-12 bg-white mb-16"
     >
-      <div className="!max-w-6xl !xl:max-w-7xl !mx-auto !px-6 sm:px-8 !lg:px-12 !xl:px-16">
-        <motion.div variants={itemVariants} className="!my-12 !text-center">
-          <h2 className="text-3xl font-semibold !leading-tight text-gray-800">
+      <div className="max-w-6xl xl:max-w-7xl mx-auto px-6 sm:px-8 lg:px-12 xl:px-16">
+        <motion.div variants={itemVariants} className="my-12 text-center">
+          <h2 className="text-3xl font-semibold leading-tight text-gray-800">
             Discover
           </h2>
-          <p className="!mt-4 font-semibold !text-lg text-gray-600">
-            Find your Dream Rental Property Today!
+          <p className="mt-4 font-semibold text-lg text-gray-600">
+            Find your Dream Rental Property Today
           </p>
-          <p className="!mt-2 text-gray-500 !max-w-3xl !mx-auto">
+          <p className="mt-2 text-gray-500 max-w-3xl mx-auto">
             Searching for your dream rental property has never been easier. With
             our user-friendly search feature, you can quickly find the perfect
             home that meets all your needs. Start your search today and discover
-            your dream rental property!
+            your dream rental property
           </p>
         </motion.div>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 !xl:gap-16 !text-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16 text-center">
           {[
             {
               imageSrc: "/landing-icon-wand.png",
@@ -83,8 +83,8 @@ const DiscoverCard = ({
   title: string;
   description: string;
 }) => (
-  <div className="!px-4 !py-12 !shadow-lg !rounded-lg bg-primary-50 md:h-64">
-    <div className="bg-primary-700 !p-[0.6rem] !rounded-full !mb-4 !h-10 !w-10 !mx-auto">
+  <div className="px-4 py-12 shadow-lg rounded-lg bg-primary-50 md:h-64">
+    <div className="bg-primary-700 p-[0.6rem] rounded-full mb-4 h-10 w-10 mx-auto">
       <Image
         src={imageSrc}
         width={30}
@@ -93,8 +93,8 @@ const DiscoverCard = ({
         alt={title}
       />
     </div>
-    <h3 className="!mt-4 text-xl font-medium text-gray-800">{title}</h3>
-    <p className="!mt-2 text-base text-gray-500">{description}</p>
+    <h3 className="mt-4 text-xl font-medium text-gray-800">{title}</h3>
+    <p className="mt-2 text-base text-gray-500">{description}</p>
   </div>
 );
 
