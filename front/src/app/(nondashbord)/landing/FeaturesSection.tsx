62bc500b1b50daeda46527b7bf955111e9a5a00e
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-03-31 11:24:03 +0000

    feature

--- front/src/app/(nondashbord)/landing/FeaturesSection.tsx
+++ front/src/app/(nondashbord)/landing/FeaturesSection.tsx
@@ -29,16 +29,16 @@ const FeaturesSection = () => {
       whileInView="visible"
       viewport={{ once: true }}
       variants={containerVariants}
-      className="!py-24 !px-6 sm:px-8 lg:px-12 !xl:px-16 bg-white"
+      className="py-24 px-6 sm:px-8 lg:px-12 xl:px-16 bg-white"
     >
-      <div className="!max-w-4xl !xl:max-w-6xl !mx-auto">
+      <div className="max-w-4xl xl:max-w-6xl mx-auto">
         <motion.h2
           variants={itemVariants}
-          className="text-3xl !font-bold text-center! mb-12! w-full! sm:w-2/3 !mx-auto"
+          className="text-3xl font-bold text-center mb-12 w-full sm:w-2/3 mx-auto"
         >
-          Quickly find the home you want using our effective search filters!
+          Quickly find the home you want using our effective search filters
         </motion.h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 !gap-8 !lg:gap-12 xl:gap-16!">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 lg:gap-12 xl:gap-16">
           {[0, 1, 2].map((index) => (
             <motion.div key={index} variants={itemVariants}>
               <FeatureCard
@@ -82,7 +82,7 @@ const FeatureCard = ({
   linkHref: string;
 }) => (
   <div className="text-center">
-    <div className="!p-4 rounded-lg !mb-4 flex !items-center justify-center !h-48">
+    <div className="p-4 rounded-lg mb-4 flex items-center justify-center h-48">
       <Image
         src={imageSrc}
         width={400}
@@ -92,10 +92,10 @@ const FeatureCard = ({
       />
     </div>
     <h3 className="text-xl font-semibold mb-2">{title}</h3>
-    <p className="!mb-4">{description}</p>
+    <p className="mb-4">{description}</p>
     <Link
       href={linkHref}
-      className="!inline-block !border border-gray-300 rounded !px-4 !py-2 hover:bg-gray-100"
+      className="inline-block border border-gray-300 rounded px-4 py-2 hover:bg-gray-100"
       scroll={false}
     >
       {linkText}
