54065c6ed3b7a21f935998646014e115756b6b49
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-05-02 22:23:00 +0100

    dashboard sidebar

---
+++ front/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+
+import Navbar from "@/components/Navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import Sidebar from "@/components/AppSidebar";
+import { NAVBAR_HEIGHT } from "@/lib/constants";
+import React, { useEffect, useState } from "react";
+import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <SidebarProvider>
+    <div className="min-h-screen w-full bg-primary-100">
+      <Navbar />
+      <div style={{ marginTop: `${NAVBAR_HEIGHT}px` }}>
+        <main className="flex">
+          <Sidebar userType={authUser.userRole.toLowerCase()} />
+          <div className="flex-grow transition-all duration-300">
+            {children}
+          </div>
+        </main>
+      </div>
+    </div>
+  </SidebarProvider>
+  )
+}
+
+export default DashboardLayout
