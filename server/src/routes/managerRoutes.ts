a19add17dd9b035c8e6540ce78f357cd9f6b4e90
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-05-07 14:09:50 +0100

    update manager controller

--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -2,13 +2,13 @@ import express from "express";
 import {
   getManager,
   createManager,
-//   updateManager,
+  updateManager,
 } from "../controllers/managerControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getManager);
-// router.put("/:cognitoId", updateManager);
+router.put("/:cognitoId", updateManager);
 router.post("/", createManager);
 // router.get("/:cognitoId/current-residences", getCurrentResidences);
 // router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
