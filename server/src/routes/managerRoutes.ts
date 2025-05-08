a5557f6f3e4e4b529e726af45bac8ae115e3267c
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-05-08 12:40:44 +0100

    getManagerProperties route

--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -3,6 +3,7 @@ import {
   getManager,
   createManager,
   updateManager,
+  getManagerProperties
 } from "../controllers/managerControllers";
 
 const router = express.Router();
@@ -10,8 +11,6 @@ const router = express.Router();
 router.get("/:cognitoId", getManager);
 router.put("/:cognitoId", updateManager);
 router.post("/", createManager);
-// router.get("/:cognitoId/current-residences", getCurrentResidences);
-// router.post("/:cognitoId/favorites/:propertyId", addFavoriteProperty);
-// router.delete("/:cognitoId/favorites/:propertyId", removeFavoriteProperty);
+router.get("/:cognitoId/properties", getManagerProperties);
 
 export default router;
