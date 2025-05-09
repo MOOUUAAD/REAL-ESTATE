97aca23b3c88d7444990d47b35a6cd4929711f66
MOOUUAAD <mouad.elmaslouhi.2003@gmail.com>
2025-05-09 13:23:33 +0100

    landing.tsx

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,6 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Landing from "./(nondashbord)/landing/page";
+
+
 export default function Home() {
   return (
-    <div>
+    <div className="h-full w-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
     </div>
   );
 }
